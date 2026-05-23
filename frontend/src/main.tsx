2bbcd0b2d30ce33df07f2616bb0d2813fa199925
Rita Castro <ritamcastro@gmail.com>
2026-05-23 18:25:51 +0100

    ✅  Add a ToDo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 
 const rootContainer: Element | DocumentFragment | null = document.getElementById('root')
 
 const ToDo = () => {
-  return <div>Hello</div>
+  const [newItem, setNewItem] = useState<string>('')
+  const [todos, setTodos] = useState<string[]>([])
+
+  const addToDo = () => {
+    setTodos([...todos, newItem])
+  }
+
+  return (
+    <div>
+      <button onClick={addToDo}>Add</button>
+      <input
+        placeholder="what needs to be done?"
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)}
+      />
+      {todos.map(item => (
+        <div>{item}</div>
+      ))}
+    </div>
+  )
 }
 
 if (rootContainer) {
