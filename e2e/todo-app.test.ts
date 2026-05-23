f21c6cecbd664a4e02b582842f6f4d814f966250
Rita Castro <ritamcastro@gmail.com>
2026-05-23 18:25:29 +0100

    🧪 Created our first test to Add a ToDo

--- e2e/todo-app.test.ts
+++ e2e/todo-app.test.ts
@@ -1,5 +1,15 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test.describe('The To-Do application', () => {
-  test('I want to add a new To-Do', async ({}) => {})
+  test('I want to add a new To-Do', async ({ page }) => {
+    await page.goto('/')
+
+    await expect(page.getByRole('button', { name: 'Add' })).toBeVisible()
+
+    await page.getByPlaceholder('what needs to be done?').fill('Write our first test')
+
+    await page.getByRole('button', { name: 'Add' }).click()
+
+    await expect(page.getByText('Write our first test')).toBeVisible()
+  })
 })
